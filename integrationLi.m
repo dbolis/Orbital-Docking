587003014b9111e2f14c6655ac5062439ee6d287
dbolis <danielbolis93@gmail.com>
2021-01-15 03:24:48 -0500

    added j2 disturbance

--- integrationLi.m
+++ integrationLi.m
@@ -1,15 +1,16 @@
-function der = integrationLi(t,x,e_v,p_v,i_v,omega_v,e_c,n_c,e_t,n_t,p_c,p_t,Ic,It,mc,mu,beta,p,q,eta,q_d,rho_d,drho_d)
-theta_t=x(1); % True anamoly 
+function der = integrationLi(t,x,e_v,p_v,i_v,omega_v,Re,J2,e_c,n_c,e_t,n_t,p_c,p_t,Ic,It,mc,mu,beta,p,q,eta,q_d,rho_d,drho_d)
+theta_v=x(1); % True anamoly 
 w_t=x(2:4); % target ang velocity
 q_t=x(5:8); % target attitude
 w_c=x(9:11);% relative angular velocity
 q_c=x(12:15)
-rho=x(16:18)
-drho=x(19:21)
-theta_v=x(22)
-rho_c=x(23:25)
-drho_c=x(26:28)
+rho_c=x(16:18)
+drho_c=x(19:21)
+rho_t=x(22:24)
+drho_t=x(25:27)
 
+rho=rho_c-rho_t
+drho=drho_c-drho_t
 
 % error=x(12:18); % error
 % derror=x(19:25); % velocity error
@@ -32,28 +33,83 @@ d2theta_v=-2*(mu/p_v^3)*(e_v*sin(theta_v))*(1+e_v*cos(theta_v))^3
 
 %%% Chaser
 
-r_v=p_v/(1+e_v*cos(theta_v)) %% virtual radius from CoM
-r_cVec_v=[r_v;0;0]+rho_c %% in virtual coordinates
-v_cVec_v=[sqrt(mu/p_v)*e_v*sin(theta_v)+drho_c(1)-dtheta_v*rho_c(2); sqrt(mu/p_v)*(1+e_v*cos(theta_v))+drho_c(2)+dtheta_v*rho_c(1); drho_c(3)] %% in virtual coordinates
+r_v=p_v/(1+e_v*cos(theta_v)) % virtual radius from CoM
+r_cVec_v=[r_v;0;0]+rho_c % chaser radius vector in virtual frame
+v_cVec_v=[sqrt(mu/p_v)*e_v*sin(theta_v)+drho_c(1)-dtheta_v*rho_c(2); sqrt(mu/p_v)*(1+e_v*cos(theta_v))+drho_c(2)+dtheta_v*rho_c(1); drho_c(3)] % chaser velocity vector in virtual frame
 
-r_cVec_ECI=transpose(r_cVec_v)*ECI2LVLH(theta_v,i_v,omega_v)
-v_cVec_ECI=transpose(v_cVec_v)*ECI2LVLH(theta_v,i_v,omega_v)
+r_cVec_ECI=transpose(r_cVec_v)*ECI2LVLH313(theta_v,i_v,omega_v) % chaser radius vector in ECI
+v_cVec_ECI=transpose(v_cVec_v)*ECI2LVLH313(theta_v,i_v,omega_v) % chaser velocity vector in ECI
 
 
+h_cVec=cross(r_cVec_ECI,v_cVec_ECI) % specific angular momentum vector
+h_cVers=h_cVec/norm(h_cVec) % specific angular momentum versor
+
+r_cVers=r_cVec_ECI/norm(r_cVec_ECI) % chaser radius versor in ECI
+theta_cVers=cross(h_cVers,r_cVers) % theta versor
+
+phi_c=asin(r_cVers(3)) % latitude
+clambda_c=r_cVers(1)/cos(phi_c) 
+slambda_c=r_cVers(2)/cos(phi_c) 
+
+lambda_c=2*atan(slambda_c/(1+clambda_c)) % absolute longitutude
+
+szeta_c=theta_cVers(3)/cos(phi_c) 
+czeta_c=h_cVers(3)/cos(phi_c)
+
+zeta_c=2*atan(szeta_c/(1+czeta_c)) % heading angle
+
+fJ2_c_c=3*Re^2*J2*mu/(norm(r_cVec_ECI))^4*[(3*sin(phi_c)^2-1)/2, -sin(phi_c)*cos(phi_c)*sin(zeta_c), -sin(phi_c)*cos(phi_c)*cos(zeta_c)] % J2 disturbance on chaser in chaser frame
+
+fJ2_c_v=fJ2_c_c*ECI2LVLH123(zeta_c, phi_c, lambda_c)*transpose(ECI2LVLH313(theta_v,i_v,omega_v)) % J2 disturbance on chaser in virtual frame
+
+%%% target
+
+
+r_tVec_v=[r_v;0;0]+rho_t  % target radius vector in virtual frame
+v_tVec_v=[sqrt(mu/p_v)*e_v*sin(theta_v)+drho_t(1)-dtheta_v*rho_t(2); sqrt(mu/p_v)*(1+e_v*cos(theta_v))+drho_t(2)+dtheta_v*rho_t(1); drho_t(3)] % target velocity vector in virtual frame
+
+r_tVec_ECI=transpose(r_tVec_v)*ECI2LVLH313(theta_v,i_v,omega_v) % target radius vector in ECI
+v_tVec_ECI=transpose(v_tVec_v)*ECI2LVLH313(theta_v,i_v,omega_v) % target velocity vector in ECI
+
+
+h_tVec=cross(r_tVec_ECI,v_tVec_ECI) % specific angular momentum vector
+h_tVers=h_tVec/norm(h_tVec) % specific angular momentum versor
+
+r_tVers=r_tVec_ECI/norm(r_tVec_ECI) % target radius versor in ECI
+theta_tVers=cross(h_tVers,r_tVers) % theta versor 
+
+phi_t=asin(r_tVers(3)) % latitude
+clambda_t=r_tVers(1)/cos(phi_t)
+slambda_t=r_tVers(2)/cos(phi_t)
+
+lambda_t=2*atan(slambda_t/(1+clambda_t)) % absolute longitutude
+
+szeta_t=theta_tVers(3)/cos(phi_t)
+czeta_t=h_tVers(3)/cos(phi_t)
+
+zeta_t=2*atan(szeta_t/(1+czeta_t)) % heading angle
+
+fJ2_t_t=3*Re^2*J2*mu/(norm(r_tVec_ECI))^4*[(3*sin(phi_t)^2-1)/2, -sin(phi_t)*cos(phi_t)*sin(zeta_t), -sin(phi_t)*cos(phi_t)*cos(zeta_t)] % J2 disturbance on target in target frame
+
+fJ2_t_v=fJ2_t_t*ECI2LVLH123(zeta_t, phi_t, lambda_t)*transpose(ECI2LVLH313(theta_v,i_v,omega_v)) % J2 disturbance on target in virtual frame
+
+
+d_c=transpose(fJ2_c_v)
+d_t=transpose(fJ2_t_v)
 
 %% Relative Orbit Dynamics
 
 % dtheta_t=(n_t*(1+e_t*cos(theta_t))^2)/((1-e_t^2)^(3/2)); % target orbital velocity
 % d2theta_t=(-2*n_t^2*e_t*sin(theta_t)*(1+e_t*cos(theta_t))^3)/((1-e_t^2)^3); % target orbital acceleration
 
-dtheta_t=sqrt(mu/p_t^3)*(1+e_t*cos(theta_t))^2
-d2theta_t=-2*(mu/p_t^3)*(e_t*sin(theta_t))*(1+e_t*cos(theta_t))^3
-
-
-r_t=p_t/(1+e_t*cos(theta_t)); % radius from Earth CoM to target
-r_cVec= [r_t;0;0]+rho; % radius from Earth CoM to chaser VECTOR Pontani coordinates
-% r_cVec= [0;0;r_t]+rho % radius from Earth CoM to chaser VECTOR Liu coordinates
-r_c= norm(r_cVec); % % radius from Earth CoM to chaser scalar
+% dtheta_t=sqrt(mu/p_t^3)*(1+e_t*cos(theta_t))^2
+% d2theta_t=-2*(mu/p_t^3)*(e_t*sin(theta_t))*(1+e_t*cos(theta_t))^3
+% 
+% 
+% r_t=p_t/(1+e_t*cos(theta_t)); % radius from Earth CoM to target
+% r_cVec= [r_t;0;0]+rho; % radius from Earth CoM to chaser VECTOR Pontani coordinates
+% % r_cVec= [0;0;r_t]+rho % radius from Earth CoM to chaser VECTOR Liu coordinates
+% r_c= norm(r_cVec); % % radius from Earth CoM to chaser scalar
 
 
 
@@ -72,23 +128,45 @@ r_c= norm(r_cVec); % % radius from Earth CoM to chaser scalar
 
 %% Pontani Coordinates
 
-X=rho(1);
-Y=rho(2);
-Z=rho(3);
+%%% chaser 
+X_c=rho_c(1);
+Y_c=rho_c(2);
+Z_c=rho_c(3);
 
-dX=drho(1);
-dY=drho(2);
-dZ=drho(3);
+dX_c=drho_c(1);
+dY_c=drho_c(2);
+dZ_c=drho_c(3);
 
-Q=((norm(rho))^2+2*transpose([r_t;0;0])*rho)/r_t^2; % EQ 13 Pontani
+Q_c=((norm(rho_c))^2+2*transpose([r_v;0;0])*rho_c)/r_v^2; % EQ 13 Pontani
 
-d2X= (mu/r_t^2)*(Q*(2+Q+(1+Q)^0.5))/((1+Q)^(3/2)*((1+Q)^0.5+1))-mu*X/(r_c^3)+d2theta_t*Y+2*dtheta_t*dY+dtheta_t^2*X; %EQ 16 Pontani
-d2Y= -mu*Y/r_c^3-d2theta_t*X-2*dtheta_t*dX+dtheta_t^2*Y; %EQ 17 Pontani
-d2Z= -mu*Z/r_c^3; %EQ 18 Pontani
+d2X_c= (mu/r_v^2)*(Q_c*(2+Q_c+(1+Q_c)^0.5))/((1+Q_c)^(3/2)*((1+Q_c)^0.5+1))-mu*X_c/((norm(r_cVec_v))^3)+d2theta_v*Y_c+2*dtheta_v*dY_c+dtheta_v^2*X_c; %EQ 16 Pontani
+d2Y_c= -mu*Y_c/((norm(r_cVec_v))^3)-d2theta_v*X_c-2*dtheta_v*dX_c+dtheta_v^2*Y_c; %EQ 17 Pontani
+d2Z_c= -mu*Z_c/((norm(r_cVec_v))^3); %EQ 18 Pontani
 
-d2rho=[d2X;d2Y;d2Z]; %d2rho 
+d2rho_c=[d2X_c;d2Y_c;d2Z_c]; %d2rho 
 
-g=mc*(-mu*r_cVec/r_c^3+mu*[r_t;0;0]/r_t^3)
+g_c=mc*(-mu*r_cVec_v/(norm(r_cVec_v)^3)+mu*r_tVec_v/(norm(r_tVec_v)^3)) %% need to rewrite!
+
+%%% target
+
+X_t=rho_t(1);
+Y_t=rho_t(2);
+Z_t=rho_t(3);
+
+dX_t=drho_t(1);
+dY_t=drho_t(2);
+dZ_t=drho_t(3);
+
+Q_t=((norm(rho_t))^2+2*transpose([r_v;0;0])*rho_t)/r_v^2; % EQ 13 Pontani
+
+d2X_t= (mu/r_v^2)*(Q_t*(2+Q_t+(1+Q_t)^0.5))/((1+Q_t)^(3/2)*((1+Q_t)^0.5+1))-mu*X_t/(norm(r_tVec_v)^3)+d2theta_v*Y_t+2*dtheta_v*dY_t+dtheta_v^2*X_t; %EQ 16 Pontani
+d2Y_t= -mu*Y_t/(norm(r_tVec_v)^3)-d2theta_v*X_t-2*dtheta_v*dX_t+dtheta_v^2*Y_t; %EQ 17 Pontani
+d2Z_t= -mu*Z_t/(norm(r_tVec_v)^3); %EQ 18 Pontani
+
+d2rho_t=[d2X_t;d2Y_t;d2Z_t]; %d2rho 
+
+
+
 
 %% Relative Attitude Dynamics
 
@@ -303,7 +381,7 @@ Tcprime = 2*transpose(T)*Tc
 %% Derivatives
 
 t
-der(1,1)=sqrt(mu/p_t^3)*(1+e_t*cos(theta_t))^2 % derivative True anamoly 
+der(1,1)=sqrt(mu/p_v^3)*(1+e_v*cos(theta_v))^2 % derivative True anamoly 
 der(2:4,1)=It\(-w_t_tilde*It*w_t) % derivative target ang velocity
 der(5:8,1)=0.5*G_t*q_t % derivative target attitude
 % if t<150
@@ -313,15 +391,15 @@ der(5:8,1)=0.5*G_t*q_t % derivative target attitude
 % end
 der(9:11,1)=Ic\(-w_c_tilde*Ic*w_c)%+Ic\Tcprime % derivative relative angular velocity
 der(12:15,1)=0.5*G_c*q_c
-der(16:18,1)=drho
-if t<95
-    der(19:21,1)= g/mc%+Fc/mc %% simulation slow after convergence.. attitude converge takes longer though..
-else
-    der(19:21,1)= g/mc
-end
-der(22,1)=dtheta_v
-der(23:25,1)=0 %placeholder
-der(26:28,1)=0 %placeholder
+der(16:18,1)=drho_c
+% if t<95
+%     der(19:21,1)= g/mc%+Fc/mc %% simulation slow after convergence.. attitude converge takes longer though..
+% else
+%     der(19:21,1)= g/mc
+% end
+der(19:21,1)=g_c/mc+Fc/mc+d_c/mc
+der(22:24,1)=drho_t
+der(25:27,1)=d2rho_t+d_t/mc
 % der(12:18,1)=derror % derivative error
 % der(19:25,1)=f%+B*u % derivative velocity error
 % der(16:19,1)=dq_r
